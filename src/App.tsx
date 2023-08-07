ca1545bfa348b06374e9ca40106e383c74c649c2
Paulo Victor Duarte <paulovictor237@gmail.com>
2023-08-06 22:39:08 -0300

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import { invoke } from '@tauri-apps/api/tauri';
+import './App.css';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
   }
 
   return (
@@ -46,6 +46,7 @@ function App() {
       </form>
 
       <p>{greetMsg}</p>
+      <h1 className="bg-red-500 text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
